e1e976d13c97934d419fdadcd5e8fee24111979e
Alberto96 <albertoguanti@gmail.com>
2015-01-28 17:22:08 +0100

    ASoC: florida: Add control for input rate
    Change-Id: I0090eb7ddce65ef4dd7d315dbdc31cadb8a358a4
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- sound/soc/codecs/arizona.c
+++ sound/soc/codecs/arizona.c
@@ -607,10 +607,19 @@ const struct soc_enum arizona_output_rate =
 	SOC_VALUE_ENUM_SINGLE(ARIZONA_OUTPUT_RATE_1,
 			      ARIZONA_OUT_RATE_SHIFT,
 			      0x0f,
-			      ARIZONA_OUT_RATE_ENUM_SIZE,
+			      ARIZONA_SYNC_RATE_ENUM_SIZE,
 			      arizona_rate_text,
 			      arizona_rate_val);
 EXPORT_SYMBOL_GPL(arizona_output_rate);
+
+const struct soc_enum arizona_input_rate =
+	SOC_VALUE_ENUM_SINGLE(ARIZONA_INPUT_RATE,
+			      ARIZONA_IN_RATE_SHIFT,
+			      0x0f,
+			      ARIZONA_SYNC_RATE_ENUM_SIZE,
+			      arizona_rate_text,
+			      arizona_rate_val);
+EXPORT_SYMBOL_GPL(arizona_input_rate);
 
 const int arizona_rate_val[ARIZONA_RATE_ENUM_SIZE] = {
 	0, 1, 2, 8,
