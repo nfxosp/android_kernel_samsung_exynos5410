8ea7e0ed66067ba61706c59c7a6243891ddf9213
Alberto96 <albertoguanti@gmail.com>
2015-01-28 15:12:52 +0100

    mfd: arizona: Document HP_CTRL_1L and HP_CTRL_1R registers
    These registers are documented in the datasheet and used as part of the
    extcon driver. Expose them properly through regmap as the datasheet
    notes they should be treated as volatile do so.
    
    Change-Id: I42a8390d6ce03bb521177526c7facca539df6eb5
    Signed-off-by: Charles Keepax <ckeepax@opensource.wolfsonmicro.com>

--- drivers/mfd/florida-tables.c
+++ drivers/mfd/florida-tables.c
@@ -2012,6 +2012,7 @@ static bool florida_readable_register(struct device *dev, unsigned int reg)
 	case ARIZONA_MIC_DETECT_1:
 	case ARIZONA_MIC_DETECT_2:
 	case ARIZONA_MIC_DETECT_3:
+	case ARIZONA_MIC_DETECT_4:
 	case ARIZONA_MIC_DETECT_LEVEL_1:
 	case ARIZONA_MIC_DETECT_LEVEL_2:
 	case ARIZONA_MIC_DETECT_LEVEL_3:
@@ -3045,6 +3046,9 @@ static bool florida_volatile_register(struct device *dev, unsigned int reg)
 	case ARIZONA_ASYNC_SAMPLE_RATE_1_STATUS:
 	case ARIZONA_ASYNC_SAMPLE_RATE_2_STATUS:
 	case ARIZONA_MIC_DETECT_3:
+	case ARIZONA_MIC_DETECT_4:
+	case ARIZONA_HP_CTRL_1L:
+	case ARIZONA_HP_CTRL_1R:
 	case ARIZONA_HEADPHONE_DETECT_2:
 	case ARIZONA_INPUT_ENABLES_STATUS:
 	case ARIZONA_OUTPUT_STATUS_1:
