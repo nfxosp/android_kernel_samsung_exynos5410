064c9ee66096b1c8967abce752644d409d565c36
Alberto96 <albertoguanti@gmail.com>
2015-05-18 23:08:55 +0200

    i9500: debug: get rid of useless sec debug/log stuff

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -84,7 +84,6 @@
 
 #include "sched.h"
 #include "../workqueue_sched.h"
-#include <mach/sec_debug.h>
 
 #define CREATE_TRACE_POINTS
 #include <trace/events/sched.h>
@@ -3261,7 +3260,6 @@ need_resched:
 	} else
 		raw_spin_unlock_irq(&rq->lock);
 
-	sec_debug_task_log(cpu, rq->curr);
 	post_schedule(rq);
 
 	sched_preempt_enable_no_resched();
@@ -6780,8 +6778,6 @@ void __init sched_init(void)
 {
 	int i, j;
 	unsigned long alloc_size = 0, ptr;
-	sec_gaf_supply_rqinfo(offsetof(struct rq, curr),
-			      offsetof(struct cfs_rq, rq));
 
 #ifdef CONFIG_FAIR_GROUP_SCHED
 	alloc_size += 2 * nr_cpu_ids * sizeof(void **);
